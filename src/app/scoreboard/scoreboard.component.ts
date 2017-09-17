8cb471c640d65cfab6ad339e742d35f200317d13
dani <dbalchev@gmail.com>
2017-09-17 12:02:26 +0300

    Initial commit

---
+++ src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+class Field {
+    constructor (
+        public name: string,
+        public label: string
+    )
+    {
+
+    }
+}
+
+const FIELDS: Array<Field> = [
+    new Field("war","Война"),
+    new Field("money","Пари"),
+    new Field("wonders","Чудеса"),
+    new Field("building","Сгради"),
+    new Field("trade","Търговия"),
+    new Field("guides","Гилдии"),
+    new Field("science","Наука"),
+    new Field("black","Черни"),
+    new Field("leaders","Лидери"),
+]
+
+class PlayerScore {
+    public scores:Array<number | null> = FIELDS.map(_ => null);
+    constructor(
+        public name: string
+    ) {
+    }
+    public sum(): number {
+        return this.scores.reduce((s, v) => s + v);
+    }
+}
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.scss']
+})
+export class ScoreboardComponent implements OnInit {
+
+  FIELDS=FIELDS;
+  private players: Array<PlayerScore> = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  addPlayer(newPlayerInput: any) {
+      this.players.push(new PlayerScore(newPlayerInput.value));
+      newPlayerInput.value = '';
+  }
+}
